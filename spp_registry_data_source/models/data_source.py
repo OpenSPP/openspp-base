0ea761b7fd11463a9e0ff411c23027a505c88f75
red <redick@newlogic.com>
2023-11-14 15:05:33 +0800

    added module spp_registry_data_source

---
+++ spp_registry_data_source/models/data_source.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+
+class SppDataSource(models.Model):
+    _name = "spp.data.source"
+    _description = "SPP Data Source"
+
+    AUTH_TYPE_CHOICES = [
+        ("basic_authentication", "Basic Authentication"),
+        ("bearer_authentication", "Bearer Authentication"),
+        ("api_keys", "API Keys"),
+    ]
+
+    name = fields.Char("Data Source Name", required=True)
+    url = fields.Char(string="Target URL", required=True)
+
+    auth_type = fields.Selection(AUTH_TYPE_CHOICES, required=True)
+
+    data_source_path_ids = fields.One2many(
+        "spp.data.source.path", "data_source_id", string="URL Paths"
+    )
