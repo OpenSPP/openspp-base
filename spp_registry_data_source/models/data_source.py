9d9c34a39deb1debf50632c9c6d7a53a829159ef
red <redick@newlogic.com>
2023-11-17 11:08:51 +0800

    add function to create data source and data source path

--- spp_registry_data_source/models/data_source.py
+++ spp_registry_data_source/models/data_source.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SppDataSource(models.Model):
@@ -23,3 +23,30 @@ class SppDataSource(models.Model):
     _sql_constraints = [
         ("name_uniq", "unique(name)", "The name of the data source must be unique !"),
     ]
+
+    @api.model
+    @api.returns("self", lambda value: value.id)
+    def create_data_source(self, vals):
+        name = vals.get("name")
+
+        data_source_id = self.env["spp.data.source"].search(
+            [("name", "=", name)], limit=1
+        )
+
+        if not data_source_id:
+            paths = []
+            if vals.get("paths"):
+                paths = vals.pop("paths")
+
+            data_source_id = self.env["spp.data.source"].create(vals)
+
+            for path in paths:
+                self.env["spp.data.source.path"].create(
+                    {
+                        "data_source_id": data_source_id.id,
+                        "name": path.get("name"),
+                        "path": path.get("path"),
+                    }
+                )
+
+        return data_source_id
